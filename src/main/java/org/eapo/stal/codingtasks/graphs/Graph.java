466463401188cfca1d57b4faf74d9c61f57b1276
Alexandr Stal <astal@eapo.org>
2016-10-21 17:35:59 +0400

    Поиск в ширину по однонаправленному графу

---
+++ src/main/java/org/eapo/stal/codingtasks/graphs/Graph.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.eapo.stal.codingtasks.graphs;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ *
+ * @author Alexandr Stal astal@eapo.org; al_stal@mail.ru
+ */
+public class Graph {
+
+    private final String name;
+    private final Set<Graph> childs;
+
+    public Graph(String name) {
+        this.name = name;
+        this.childs = new HashSet<>();
+    }
+
+    public Set<Graph> getChilds() {
+        return this.childs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Graph other = (Graph) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static boolean isLink(Graph a, Graph b) {
+        Queue<Graph> list = new LinkedList<>();
+        list.add(a);
+
+        HashSet<Graph> set = new HashSet<>();
+      
+        while (!list.isEmpty()) {
+            Graph g = list.poll();
+            if (!set.contains(g)) {
+                set.add(g);
+                if (g.getName().equals(b.getName())) {
+                    return true;
+                }
+                list.addAll(g.getChilds()); 
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Graph a = new Graph("a");
+        Graph b = new Graph("b");
+        Graph c = new Graph("c");
+        Graph d = new Graph("d");
+
+        Graph e = new Graph("e");
+        Graph f = new Graph("f");
+        Graph g = new Graph("j");
+        Graph h = new Graph("h");
+
+        a.getChilds().add(b);
+        b.getChilds().add(c);
+        b.getChilds().add(a);
+        b.getChilds().add(d);
+        
+        d.getChilds().add(e);
+        e.getChilds().add(g);
+        e.getChilds().add(h);
+
+
+    }
+
+}
